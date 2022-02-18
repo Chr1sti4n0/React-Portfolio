feba0627c0d2308d4ebf09b27bb09fdbc170512c
Chris Jimenez <chrisjimenez8118@gmail.com>
2022-02-18 14:06:24 -0700

    Updated About js and css files.

--- 00-react-portfolio-app/src/components/pages/About.js
+++ 00-react-portfolio-app/src/components/pages/About.js
@@ -1,17 +1,17 @@
 import React from 'react';
-// import profile from '';
-import '../pages/About.css'
+import Profile from '../../images/photo_of_me.png'
+import '../pages/About.css';
 
 function About() {
   return (
     <div>
-      <h1>About Me</h1>
+      <h1 className="about-me" >About Me</h1>
       <p>
       My name is Christian Jimenez and I am a full-stack software developer. This is my portfolio which I built
       using React. In this portfolio you will find my contact information, a list of a few projects I 
       have worked on, their repositories, a link to my GitHub and my resume. 
       </p>
-      {/* <img src={profile} alt="Photo of Me" /> */}
+      <img src={Profile} alt="Photo of Me" />
     </div>
   );
 }
