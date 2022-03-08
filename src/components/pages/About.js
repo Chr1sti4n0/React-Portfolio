9b6409346108e2dfb8efea456332e5c755f4cdbf
Chris Jimenez <chrisjimenez8118@gmail.com>
2022-03-08 11:22:29 -0700

    Updated Resume section.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,9 +7,11 @@ function About() {
     <div className='about-me-page'>
       <h1 className="about-me" >About Me</h1>
       <p className='story'>
-      My name is Christian Jimenez and I am a full-stack software developer. This is my portfolio which I built
+      My name is Christian Jimenez and I am a full-stack software developer. This is my portfolio which was built
       using React. In this portfolio you will find my contact information, a list of a few projects I 
-      have worked on, their repositories, a link to my GitHub and my resume. 
+      have worked on, their repositories, a link to my GitHub and my resume. My educational background includes a Bachelor of 
+      Science in Engineering focused in Mechanical Engineering from Arizona State University and a Master of Business 
+      Administration from Grand Canyon University.
       </p>
       <img src={Profile} alt="Photo of Me" />
     </div>
